8fece97072d0ddc020acf12c94f3e524b49039f4
xcel24 <prateekgoswami1996@gmail.com>
2024-07-07 12:15:57 +0530

    first python project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 birth_year = input("What is your birth year? ")
+name = input("What is your name? ")
 age = 2024 - int(birth_year)
 
-print("your age is", age)
+print("your name and age is", name, age)
